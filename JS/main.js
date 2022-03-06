fedd0ddc777b2b44fb69ba2869cfd13d1570200e
Jograj singh <jukeshundal@gmail.com>
2022-03-06 13:24:42 +0000

    Webpack 5 bugfix.

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,6 @@
 import style from '/SCSS/style.scss';
-import  MappEvents,{CalorieEvents,NavEvents} from '/JS/Event.js'
+import  MappEvents,{CalorieEvents} from '/JS/Event.js';
+import {NavEvents} from '/JS/Event.js';
 let mapEvent = new MappEvents();
 let calorieEvents = new CalorieEvents();
 let navEvents = new NavEvents();
